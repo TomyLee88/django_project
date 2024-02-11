a9f5b24c16bcf1abc32ce7125b76498dc5fc11f7
TomyLee88 <78692597+TomyLee88@users.noreply.github.com>
2024-02-11 02:34:10 +0000

    Update settings.py
    adding static url again

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+STATIC_URL = "static/" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
